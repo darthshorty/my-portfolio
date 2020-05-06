a07c637d91012edf4d7ca6f310e10e2b1f1e8ba9
Steve Shortman <darthshorty@gmail.com>
2020-05-06 11:03:02 +0100

    use SNS to notify portfolio is deployed

--- upload-portfolio-lamda.py
+++ upload-portfolio-lamda.py
@@ -6,21 +6,29 @@ import mimetypes
 import json
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:eu-west-2:462443447184:deployPortfolioTopic')
 
-    portfolio_bucket = s3.Bucket('portfolio.steveshortman.co.uk')
-    build_bucket = s3.Bucket('portfoliobuild.steveshortman.co.uk')
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.steveshortman.co.uk')
+        build_bucket = s3.Bucket('portfoliobuild.steveshortman.co.uk')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    print "job done!"        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print 'Job done!'
+        topic.publish(Subject="Portfolio deployed", Message="Portfolio deployed successfully.")
+    except:
+        topic.publish(Subject="Portfolio deploy failed", Message="Portfolio was not deployed successfully.")
+        raise
 
     return 'Hello from Lambda!'
